d5cac2692bfdd481c8083285dedd967053df0e5c
Araiva Viruskizz <kizzaraiva@gmail.com>
2020-11-22 03:44:25 +0700

    homepage

--- src/app/screens/home/info/info.component.ts
+++ src/app/screens/home/info/info.component.ts
@@ -9,24 +9,34 @@ export class InfoComponent implements OnInit {
 
   info = [
     {
-      imgUrl: 'assets/images/services-icon-1.svg',
+      imgUrl: 'assets/images/geekinu-info-level.png',
       title: 'ระดับการศึกษา',
       description: 'นักเรียนชั้นมัธยมศึกษาปีที่6 หรือนักเรียนปวช.3 ที่มีสนใจเทคโนโลยีสารสนเทศและอยากจะเข้าศึกษาต่อในสายงานด้านเทคโนโลยีสารสนเทศ',
     },
     {
-      imgUrl: 'assets/images/services-icon-3.svg',
+      imgUrl: 'assets/images/geekinu-info-cert.png',
       title: 'มีประกาศนียบัตรให้หรือไม่?',
       description: 'มีประกาศนียบัตรให้กับผู้เข้าร่วมกิจกรรม'
     },
     {
-      imgUrl: 'assets/images/services-icon-3.svg',
+      imgUrl: 'assets/images/geekinu-info-price.png',
       title: 'ค่าใช้จ่ายในการเข้าร่วมกิจกรรม?',
       description: 'มีค่าลงทะเบียน 690 บาท'
     },
     {
-      imgUrl: 'assets/images/services-icon-3.svg',
+      imgUrl: 'assets/images/geekinu-info-person.png',
       title: 'จำนวนที่รับ',
       description: 'ออฟไลน์ จำนวน 50-70 คน'
+    },
+    {
+      imgUrl: 'assets/images/geekinu-info-privilege.png',
+      title: 'สิทธิพิเศษ',
+      description: 'ฟรีสำหรับ 5 ท่านแรก'
+    },
+    {
+      imgUrl: 'assets/images/geekinu-info-swag.png',
+      title: 'ของที่ระลึก',
+      description: 'เสื้อกิจกรรมและอื่นๆ'
     }
   ];
   constructor() { }
